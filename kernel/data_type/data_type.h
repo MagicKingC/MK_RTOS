7799ec69a82203cb620d1fa4ea1e89526eb26a1b
MagicKing <1920417572@qq.com>
2021-02-27 00:45:44 +0800

    第一次提交项目

---
+++ kernel/data_type/data_type.h
@@ -0,0 +1,15 @@
+#ifndef DATA_TYPE_H
+#define DATA_TYPE_H
+
+//RTOS的自定义数据类型
+
+typedef unsigned int 	 mk_uint32;
+typedef unsigned short mk_uint16;
+typedef unsigned char  mk_uint8;
+
+typedef int 	mk_int32;
+typedef short mk_int16;
+typedef char  mk_int8;
+
+
+#endif
