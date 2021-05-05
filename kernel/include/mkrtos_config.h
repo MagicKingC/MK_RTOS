356c512ba02d4c2b013149a3b44c732acd055201
MagicKing <1920417572@qq.com>
2021-05-05 20:29:34 +0800

    加入了临界区保护机制

--- kernel/include/mkrtos_config.h
+++ kernel/include/mkrtos_config.h
@@ -4,4 +4,7 @@
 //时间戳
 #define USE_TIME_STAMP 0	//关闭
 
+//支持最大的优先级
+#define MR_PRIORITY_MAX	32
+
 #endif
---
+++ kernel/source/priority.c
@@ -0,0 +1,12 @@
+#include <priority.h>
+#include <mkrtos_config.h>
+#include <data_type.h>
+
+//优先级表
+mk_uint32 _MK_PRIO_TABLE_[MR_PRIORITY_MAX];
+
+void _MK_PrioInit_(void){
+
+}
+
+
