f15ff7e09f0764e036066a82f88f04065c9d0f41
MagicKingC <magickingc@qq.com>
2025-05-05 02:23:21 +0800

    修改了测试例子

--- app/main.c
+++ app/main.c
@@ -7,8 +7,8 @@ mk_sem_t sem;
 mk_task_t task1;
 mk_task_t task2;
 
-static mk_stack_t task1_stk[1024];
-static mk_stack_t task2_stk[1024];
+static mk_stack_t task1_stk[128];
+static mk_stack_t task2_stk[128];
 
 void task1_entry(void* param) {
     mkprintk("%s\r\n", __func__);
@@ -40,18 +40,19 @@ void test_time2(void* param) {
  * @return int 
  */
 int main(void) {
+    const char* test = "test";
     mkprintk("entry main\r\n");
     mkprintk("now task name: %s\r\n", mk_get_current_task_name());
 
     mk_sem_create(&sem, "test_sem", 0);
 
-    mk_timer_t tmp_timer1 = mk_timer_create("test_timer1", test_time1, (void *) 1, 100, MK_TIMER_REPEAT);
+    mk_timer_t tmp_timer1 = mk_timer_create("test_timer1", test_time1, (void *) 4, 100, MK_TIMER_REPEAT);
     mk_timer_t tmp_timer2 = mk_timer_create("test_timer2", test_time2, MK_NULL, 100, MK_TIMER_ONCE);
 
     mk_start_timer(&tmp_timer1);
     mk_start_timer(&tmp_timer2);
 
-    mk_task_init("test1", &task1, task1_entry, (void*)0x0, task1_stk, 1024, 2, 10);
+    mk_task_init("test1", &task1, task1_entry, (void*)test, task1_stk, 1024, 2, 10);
     mk_task_init("test2", &task2, task2_entry, (void*)0x0, task2_stk, 1024, 2, 10);
 
     mk_task_start(&task1);
